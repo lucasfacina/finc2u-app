a348b3e431100d804eca3e765ed34cbe3bd4086c
Lucas Facina <facina3@hotmail.com>
2026-06-22 15:46:17 -0300

    chore: ✨ estrutura do monorepo e scaffold do backend Spring Boot

---
+++ apps/server/src/main/java/br/com/finc2u/server/Finc2UAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.finc2u.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finc2UAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Finc2UAppApplication.class, args);
+    }
+
+}
---
+++ apps/server/src/test/java/br/com/finc2u/server/Finc2UAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finc2u.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finc2UAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
