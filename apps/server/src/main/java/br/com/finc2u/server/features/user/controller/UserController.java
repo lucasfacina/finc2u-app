eb3dee18609f8fa45354e1887f4ee2d96b0e80da
Lucas Facina <facina3@hotmail.com>
2026-06-23 13:24:44 -0300

    feat: ✨ criar classe `UserController`

---
+++ apps/server/src/main/java/br/com/finc2u/server/features/user/controller/UserController.java
@@ -0,0 +1,69 @@
+package br.com.finc2u.server.features.user.controller;
+
+import br.com.finc2u.server.features.user.dto.UserRequest;
+import br.com.finc2u.server.features.user.entity.User;
+import br.com.finc2u.server.features.user.entity.UserConfiguration;
+import br.com.finc2u.server.features.user.form.UserResponse;
+import br.com.finc2u.server.features.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> create(@Valid @RequestBody UserRequest userRequest) {
+        User user = toEntity(userRequest);
+        User savedUser = userService.create(user);
+        return ResponseEntity.ok(UserResponse.from(savedUser));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAll() {
+        List<UserResponse> response = userService.getAll()
+                .stream()
+                .map(UserResponse::from)
+                .toList();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getById(@PathVariable UUID id) {
+        User user = userService.getById(id);
+        return ResponseEntity.ok(UserResponse.from(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable UUID id, @Valid @RequestBody UserRequest userRequest) {
+        User userUpdates = toEntity(userRequest);
+        User updatedUser = userService.update(id, userUpdates);
+        return ResponseEntity.ok(UserResponse.from(updatedUser));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    private User toEntity(UserRequest userRequest) {
+        return new User(
+                userRequest.name(),
+                userRequest.email(),
+                new UserConfiguration(
+                        userRequest.baseSalary(),
+                        userRequest.savingsBalance()
+                )
+        );
+    }
+
+}
