7383c4e4d19adb9eeb16cc1ccbd2340093b2cc97
Lucas Facina <facina3@hotmail.com>
2026-06-24 11:53:16 -0300

    feat: ✨ adicionar gerenciamento de `CardAccount` no `SeedContext`

--- apps/server/src/main/java/br/com/finc2u/server/shared/seed/SeedContext.java
+++ apps/server/src/main/java/br/com/finc2u/server/shared/seed/SeedContext.java
@@ -1,13 +1,27 @@
 package br.com.finc2u.server.shared.seed;
 
+import br.com.finc2u.server.features.card.entity.CardAccount;
 import br.com.finc2u.server.features.user.entity.User;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Getter
 @Setter
 public class SeedContext {
 
     private User user;
+
+    private final Map<String, CardAccount> cardsByName = new LinkedHashMap<>();
+
+    public void putCard(CardAccount card) {
+        cardsByName.put(card.getCardName(), card);
+    }
+
+    public CardAccount card(String name) {
+        return cardsByName.get(name);
+    }
 
 }
