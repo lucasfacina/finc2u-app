a324e7fa0c1a1a0dcbf6f2a01c8405cae8e72e4f
Lucas Facina <facina3@hotmail.com>
2026-06-24 15:16:02 -0300

    feat: ✨ criar classe `TagService`

---
+++ apps/server/src/main/java/br/com/finc2u/server/features/tag/service/TagService.java
@@ -0,0 +1,61 @@
+package br.com.finc2u.server.features.tag.service;
+
+import br.com.finc2u.server.exception.BusinessException;
+import br.com.finc2u.server.exception.ResourceNotFoundException;
+import br.com.finc2u.server.features.tag.entity.Tag;
+import br.com.finc2u.server.features.tag.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TagService {
+
+    private final TagRepository tagRepository;
+
+    @Transactional
+    public Tag create(Tag tag) {
+        if (tagRepository.findByNameIgnoreCase(tag.getName()).isPresent()) {
+            throw new BusinessException("Tag já cadastrada");
+        }
+
+        return tagRepository.save(tag);
+    }
+
+    @Transactional(readOnly = true)
+    public Tag getById(UUID id) {
+        return tagRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Tag não encontrada com o ID: " + id));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Tag> getAll() {
+        return tagRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Tag> getByIds(List<UUID> ids) {
+        return tagRepository.findAllById(ids);
+    }
+
+    @Transactional
+    public Tag update(UUID id, Tag tagUpdated) {
+        Tag tag = getById(id);
+
+        tag.setName(tagUpdated.getName());
+        tag.setColorCode(tagUpdated.getColorCode());
+
+        return tagRepository.save(tag);
+    }
+
+    @Transactional
+    public void delete(UUID id) {
+        Tag tag = getById(id);
+        tagRepository.delete(tag);
+    }
+
+}
