3afd5ba501887c66776cc1f8c6fa11908498fb94
Lucas Facina <facina3@hotmail.com>
2026-06-24 09:48:05 -0300

    feat: ✨ adicionar anotação `@Repository` na interface `UserRepository`

--- apps/server/src/main/java/br/com/finc2u/server/features/user/repository/UserRepository.java
+++ apps/server/src/main/java/br/com/finc2u/server/features/user/repository/UserRepository.java
@@ -2,11 +2,13 @@ package br.com.finc2u.server.features.user.repository;
 
 import br.com.finc2u.server.features.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
