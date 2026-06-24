64971b7f36bfc50fde9bfe9ef78b0c028fb72be2
Lucas Facina <facina3@hotmail.com>
2026-06-24 14:43:23 -0300

    feat: ✨ criar entidade `Tag`

---
+++ apps/server/src/main/java/br/com/finc2u/server/features/tag/entity/Tag.java
@@ -0,0 +1,20 @@
+package br.com.finc2u.server.features.tag.entity;
+
+import br.com.finc2u.server.shared.model.BaseModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table("tb_tag")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tag extends BaseModel {
+
+    private String name;
+    private Long colorCode;
+
+}
