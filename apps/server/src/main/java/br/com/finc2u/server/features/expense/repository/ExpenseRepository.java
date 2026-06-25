ea506b76d8fea2d927618dc3773e90d9be74c802
Lucas Facina <facina3@hotmail.com>
2026-06-25 10:56:18 -0300

    feat: ✨️ criar interface `ExpenseRepository`

---
+++ apps/server/src/main/java/br/com/finc2u/server/features/expense/repository/ExpenseRepository.java
@@ -0,0 +1,45 @@
+package br.com.finc2u.server.features.expense.repository;
+
+import br.com.finc2u.server.features.expense.entity.Expense;
+import br.com.finc2u.server.features.expense.enums.ExpenseType;
+import br.com.finc2u.server.features.expense.enums.PaymentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+
+    List<Expense> findByUserId(UUID userId);
+
+    List<Expense> findByCardAccountId(UUID cardAccountId);
+
+    List<Expense> findByPaymentStatus(PaymentStatus status);
+
+    List<Expense> findByExpenseTypeAndPaymentStatus(ExpenseType type, PaymentStatus status);
+
+    List<Expense> findByPaymentStatusOrExpenseType(PaymentStatus status, ExpenseType type);
+
+    List<Expense> findByCardAccountIdIn(List<UUID> cardAccountIds);
+
+    List<Expense> findByUserIdOrderByValueDesc(UUID userId);
+
+    List<Expense> findByDueDateBetween(LocalDate start, LocalDate end);
+
+    List<Expense> findByUserIdAndDueDateBetween(UUID userId, LocalDate start, LocalDate end);
+
+    List<Expense> findByValueLessThan(BigDecimal value);
+
+    List<Expense> findByValueLessThanEqual(BigDecimal value);
+
+    List<Expense> findByValueGreaterThan(BigDecimal value);
+
+    List<Expense> findByValueGreaterThanEqual(BigDecimal value);
+
+    List<Expense> findByExpenseTypeAndUserId(ExpenseType type, UUID userId);
+
+}
