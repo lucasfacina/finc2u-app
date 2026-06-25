a342ed18a1738adec1544a4f0f939fa98fb12fe2
Lucas Facina <facina3@hotmail.com>
2026-06-25 10:28:17 -0300

    feat: ✨ adicionar relacionamento com `Expense` na entidade `CardAccount`

--- apps/server/src/main/java/br/com/finc2u/server/features/card/entity/CardAccount.java
+++ apps/server/src/main/java/br/com/finc2u/server/features/card/entity/CardAccount.java
@@ -1,11 +1,16 @@
 package br.com.finc2u.server.features.card.entity;
 
+import br.com.finc2u.server.features.expense.entity.Expense;
 import br.com.finc2u.server.features.user.entity.User;
 import br.com.finc2u.server.shared.model.BaseModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_card_account")
@@ -22,5 +27,9 @@ public class CardAccount extends BaseModel {
 
     @ManyToOne
     private User user;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cardAccount")
+    private List<Expense> expenseList;
 
 }
