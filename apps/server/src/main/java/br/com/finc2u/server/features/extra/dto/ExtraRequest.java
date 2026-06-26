1a8e10f9e4e3a736170fbec8df04b7693fd63753
Lucas Facina <facina3@hotmail.com>
2026-06-26 10:14:13 -0300

    feat: ✨ criar record `ExtraRequest`

---
+++ apps/server/src/main/java/br/com/finc2u/server/features/extra/dto/ExtraRequest.java
@@ -0,0 +1,35 @@
+package br.com.finc2u.server.features.extra.dto;
+
+import br.com.finc2u.server.features.extra.entity.Extra;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record ExtraRequest(
+        @NotNull(message = "O ID do usuário é obrigatório")
+        UUID userId,
+
+        @NotBlank(message = "Descrição é obrigatória")
+        String description,
+
+        @NotNull(message = "Valor é obrigatório")
+        @Positive(message = "Valor deve ser positivo")
+        BigDecimal value,
+
+        @NotNull(message = "Data é obrigatória")
+        LocalDate date
+) {
+
+    public Extra toEntity() {
+        return Extra.builder()
+                .description(this.description())
+                .value(this.value())
+                .date(this.date())
+                .build();
+    }
+
+}
