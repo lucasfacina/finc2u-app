d30e28db944bbd47ccd00506a62ac693f0388462
Lucas Facina <facina3@hotmail.com>
2026-06-26 10:08:15 -0300

    feat: ✨ adicionar relacionamento com `Extra` na entidade `User`

--- apps/server/src/main/java/br/com/finc2u/server/features/user/entity/User.java
+++ apps/server/src/main/java/br/com/finc2u/server/features/user/entity/User.java
@@ -1,6 +1,7 @@
 package br.com.finc2u.server.features.user.entity;
 
 import br.com.finc2u.server.features.card.entity.CardAccount;
+import br.com.finc2u.server.features.extra.entity.Extra;
 import br.com.finc2u.server.shared.model.BaseModel;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -26,5 +27,9 @@ public class User extends BaseModel {
     @JsonIgnore
     @OneToMany(mappedBy = "user")
     List<CardAccount> cardAccountList;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    List<Extra> extraList;
 
 }
