6ccb9664258aa933ff70e61d5c4f57f92c5a6214
Lucas Facina <facina3@hotmail.com>
2026-06-26 11:27:56 -0300

    feat: ✨ adicionar relacionamento com `MonthlySummary` na entidade `User`

--- apps/server/src/main/java/br/com/finc2u/server/features/user/entity/User.java
+++ apps/server/src/main/java/br/com/finc2u/server/features/user/entity/User.java
@@ -2,6 +2,7 @@ package br.com.finc2u.server.features.user.entity;
 
 import br.com.finc2u.server.features.card.entity.CardAccount;
 import br.com.finc2u.server.features.extra.entity.Extra;
+import br.com.finc2u.server.features.summary.entity.MonthlySummary;
 import br.com.finc2u.server.shared.model.BaseModel;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -31,5 +32,9 @@ public class User extends BaseModel {
     @JsonIgnore
     @OneToMany(mappedBy = "user")
     List<Extra> extraList;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    List<MonthlySummary> monthlySummaryList;
 
 }
